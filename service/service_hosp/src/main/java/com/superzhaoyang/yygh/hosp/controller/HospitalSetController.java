d74ad45e643549bd74be253f116344a7bc910236
superzhaoyang <1599602677@qq.com>
2021-11-22 13:54:14 +0800

    first commit

--- service/service_hosp/src/main/java/com/superzhaoyang/yygh/hosp/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/com/superzhaoyang/yygh/hosp/controller/HospitalSetController.java
@@ -1,8 +1,10 @@
 package com.superzhaoyang.yygh.hosp.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.api.R;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.superzhaoyang.yygh.common.result.Result;
+import com.superzhaoyang.yygh.common.utils.MD5;
 import com.superzhaoyang.yygh.hosp.service.HospitalSetService;
 import com.superzhaoyang.yygh.model.hosp.HospitalSet;
 import com.superzhaoyang.yygh.vo.hosp.HospitalSetQueryVo;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.management.Query;
 import java.util.List;
+import java.util.Random;
 
 @Api(tags = "医院设置管理")
 @RestController
@@ -71,6 +74,21 @@ public class HospitalSetController {
         return Result.ok(pageHospitalSet);
     }
     // 4 添加医院设置
+    @PostMapping("saveHospitalSet")
+    public Result saveHospitalSet(@RequestBody HospitalSet hospitalSet) {
+        //设置状态1使用 0不使用
+        hospitalSet.setStatus(1);
+        //签名密钥
+        Random random = new Random();
+        hospitalSet.setSignKey(MD5.encrypt(System.currentTimeMillis()+""+ random.nextInt(1000)));
+        //调用service
+        boolean save = hospitalSetService.save(hospitalSet);
+        if (save) {
+            return Result.ok();
+        } else {
+            return Result.fail();
+        }
+    }
 
     // 5 根据id获取医院设置
 
